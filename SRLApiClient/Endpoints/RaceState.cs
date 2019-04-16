4db45b7857392e93f64c2ef84c9f3cf56b237bb3
Matteias Collet <matteias.collet@bluewin.ch>
2019-04-16 19:01:58 +0200

    seperate enums in SRLApiClient.Endpoints

---
+++ SRLApiClient/Endpoints/RaceState.cs
@@ -0,0 +1,39 @@
+﻿namespace SRLApiClient.Endpoints
+{
+  /// <summary>
+  /// The current state of the race
+  /// </summary>
+  public enum RaceState : uint
+  {
+    /// <summary>
+    /// Fallback value
+    /// </summary>
+    Unknown = 0,
+
+    /// <summary>
+    /// The race is open for players
+    /// </summary>
+    EntryOpen = 1,
+
+    /// <summary>
+    /// The race is closed but the race hasn't started yet
+    /// (This is where the 10 second countdown happens)
+    /// </summary>
+    EntryClosed = 2,
+
+    /// <summary>
+    /// The race is in progress
+    /// </summary>
+    InProgress = 3,
+
+    /// <summary>
+    /// The race has finished but hasn't been recorded yet
+    /// </summary>
+    Finished = 4,
+
+    /// <summary>
+    /// The race has been recorded
+    /// </summary>
+    Over = 5
+  }
+}
---
+++ SRLApiClient/Endpoints/StreamApi.cs
@@ -0,0 +1,24 @@
+﻿namespace SRLApiClient.Endpoints
+{
+  /// <summary>
+  /// The API used for fetching streams. The according
+  /// channel can usually be found in the 'Channel' Property
+  /// </summary>
+  public enum StreamApi
+  {
+    /// <summary>
+    /// Fallback value
+    /// </summary>
+    Unknown,
+
+    /// <summary>
+    /// Twitch API
+    /// </summary>
+    Twitch,
+
+    /// <summary>
+    /// Hitbox API
+    /// </summary>
+    Hitbox
+  }
+}
